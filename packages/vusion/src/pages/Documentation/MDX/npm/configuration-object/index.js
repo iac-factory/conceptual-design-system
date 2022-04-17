6afcfe59bc7fc34d6570b9d24b0940b7b4110a04
Jacob B. Sanders <jacob.sanders@users.noreply.github.com>
2022-04-17 11:39:05 -0500

    [Development] - Inline-Documentation

--- packages/vusion/src/pages/Documentation/MDX/npm/configuration-object/index.js
+++ packages/vusion/src/pages/Documentation/MDX/npm/configuration-object/index.js
@@ -1,141 +1,18 @@
 import Content from "./index.mdx";
 
-import Highlighter from "react-syntax-highlighter";
+import { Mutators } from "..";
 
-const pastel = {
-    "hljs": {
-        "display": "block",
-        "overflowX": "auto",
-        "padding": "0.5em",
-        "color": "#FFFFFF"
-    },
-    "hljs-comment": {
-        "color": "#5C6370",
-        "fontStyle": "italic"
-    },
-    "hljs-quote": {
-        "color": "#FFFFFF",
-        "fontStyle": "italic"
-    },
-    "hljs-doctag": {
-        "color": "#F39DE8"
-    },
-    "hljs-keyword": {
-        "color": "#F39DE8"
-    },
-    "hljs-formula": {
-        "color": "#F39DE8"
-    },
-    "hljs-section": {
-        "color": "#F39D9D"
-    },
-    "hljs-name": {
-        "color": "#F39D9D"
-    },
-    "hljs-selector-tag": {
-        "color": "#F39D9D"
-    },
-    "hljs-deletion": {
-        "color": "#F39D9D"
-    },
-    "hljs-subst": {
-        "color": "#F39D9D"
-    },
-    "hljs-literal": {
-        "color": "#F39D9D"
-    },
-    "hljs-string": {
-        "color": "#9DF3B8"
-    },
-    "hljs-regexp": {
-        "color": "#9DF3B8"
-    },
-    "hljs-addition": {
-        "color": "#9DF3B8"
-    },
-    "hljs-attribute": {
-        "color": "#9DF3B8"
-    },
-    "hljs-meta-string": {
-        "color": "#9DF3B8"
-    },
-    "hljs-built_in": {
-        "color": "#9DD9F3"
-    },
-    "hljs-class .hljs-title": {
-        "color": "#9DD9F3"
-    },
-    "hljs-attr": {
-        "color": "#BA9DF3"
-    },
-    "hljs-variable": {
-        "color": "#BA9DF3"
-    },
-    "hljs-template-variable": {
-        "color": "#BA9DF3"
-    },
-    "hljs-type": {
-        "color": "#BA9DF3"
-    },
-    "hljs-selector-class": {
-        "color": "#BA9DF3"
-    },
-    "hljs-selector-attr": {
-        "color": "#BA9DF3"
-    },
-    "hljs-selector-pseudo": {
-        "color": "#BA9DF3"
-    },
-    "hljs-number": {
-        "color": "#BA9DF3"
-    },
-    "hljs-symbol": {
-        "color": "#618EEE"
-    },
-    "hljs-bullet": {
-        "color": "#618EEE"
-    },
-    "hljs-link": {
-        "color": "#618EEE",
-        "textDecoration": "underline"
-    },
-    "hljs-meta": {
-        "color": "#618EEE"
-    },
-    "hljs-selector-id": {
-        "color": "#618EEE"
-    },
-    "hljs-title": {
-        "color": "#618EEE"
-    },
-    "hljs-emphasis": {
-        "fontStyle": "italic"
-    },
-    "hljs-strong": {
-        "fontWeight": "bold"
-    }
-};
-
+/***
+ * The MDX (Markdown) Content + Runtime Injections
+ * ---
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const Component = () => {
-    const code = ({ className, children, ... properties }) => {
-        const match = /language-(\w+)/.exec(className || "");
-        console.log(properties, className);
-        return (match?.[1]) ? (
-            <Highlighter
-                children={ String(children).replace(/\n$/, "") }
-                style={ pastel }
-                language={ match[1] ?? null }
-                PreTag="div"
-                { ... properties }
-            />
-        ) : (
-            <code>
-                { children }
-            </code>
-        )
-    };
+    const code = Mutators.code;
 
-    /// Interesting... Components has to be := an html tag <code> for example
+    /*** `components` has to be := an html tag (.e.g. <code> - relating to a {@link Mutators} extension */
     const Target = () => (<Content components={ { code } }/>);
 
     return (<Target/>);
--- packages/vusion/src/pages/Documentation/MDX/npm/index.js
+++ packages/vusion/src/pages/Documentation/MDX/npm/index.js
@@ -1,7 +1,17 @@
 import { lazy } from "react";
 
 import { Content } from "..";
+import { Mutators } from "..";
 
+/***
+ * A 2nd Level Abstraction for MDX Component(s) relating to `npm` Imports/Exports
+ * ---
+ *
+ * See {@link Content} for the the parent
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const Configuration = () => {
     const Component = lazy(() => import("./configuration-object"));
     return (
@@ -14,5 +24,6 @@ const NPM = {
 };
 
 export { NPM };
+export { Mutators };
 
 export default NPM;
